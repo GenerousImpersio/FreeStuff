861cdcb9a124c19833c17372e7c4b75d1a4db980
Can Sönmez <60190086+GenerousImpersio@users.noreply.github.com>
2023-05-10 18:42:36 +0300

    Delete generous_inventory/generous_framework-main directory

--- generous_inventory/generous_framework-main/src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-import Config from "../config.json";
-
-export function isEmpty(obj) {
-    return obj && Object.keys(obj).length === 0 && obj.constructor === Object;
-}
-
-export function IsItAWeapon(itemName:string) {
-    return itemName.includes(Config.weaponPrefix);
-}
